7fc51b7260d5beb50334c15c02206cc0eada9cf9
Osita <ositaodunze2@gmail.com>
2024-06-04 14:08:11 -0500

    Create Motion.py

---
+++ Motion.py
@@ -0,0 +1,18 @@
+import cyberpi
+import time
+
+motion_threshold = 2  
+
+while True:
+    motion_value = cyberpi.pocket.read_analog("s2")  
+    if motion_value > motion_threshold:
+       
+        cyberpi.pocket.write_digital(1, "s1")  
+        time.sleep(0.5)  
+        cyberpi.pocket.write_digital(0, "s1")  
+        time.sleep(0.5)  
+    else:
+        cyberpi.pocket.write_digital(0, "s1")  
+    
+    cyberpi.console.println(motion_value)  
+    time.sleep(1)  
