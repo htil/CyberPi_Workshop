909c6767710ddf9a7c97b50d18f91f18427ad2e0
Chris Crawford <chris35469@gmail.com>
2025-06-08 11:36:52 -0500

    Update boxplot

--- boxplot.py
+++ boxplot.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
  
-file_name = "data4.csv"
+file_name = "shake.csv"
 df = pd.read_csv(file_name)
  
 sns.boxplot(x = 'Label', y = 'Shake_Value', hue="Label", palette=["m", "g"], data = df)
